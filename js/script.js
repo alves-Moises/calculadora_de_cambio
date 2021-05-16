fe9f9ccbf1f689fbadb3afc4cfb4954d77b9cece
alves-Moises <moisesingo_ra@hotmail.com>
2021-05-16 18:23:58 -0300

    fix: money prefix

--- js/script.js
+++ js/script.js
@@ -26,20 +26,7 @@ function calculate() {
     });
 }
 
-// function prefix(){
-//   if(prefix_one.value == "BRL"){
-//     prefix_one.value = "R$";
-//   } else if(prefix_one.value == "EUR"){
-//     prefix_one.value = "€";
-//   }else if(prefix_one.value == "INR"){
-//     prefix_one.value = "₹";
-//   }else if(prefix_one.value == "USD"){
-//     prefix_one.value = "$";
-//   }else {
-//     prefix_one.value = "i!error!i"; 
-//   }
-// }
-// alert values
+
 
 
 // function d_calc() {
@@ -50,25 +37,49 @@ function calculate() {
 // }
 
 function rel(){
-  
-  
-  
+  // =========== Data atual =====================
+  var data = new Date(),
+        dia  = data.getDate().toString(),
+        diaF = (dia.length == 1) ? '0'+dia : dia,
+        mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+        mesF = (mes.length == 1) ? '0'+mes : mes,
+        anoF = data.getFullYear();
 
+        coin_one = document.getElementById("currency-one");
+        coin_two = document.getElementById("currency-two");
+        
+  //  =========== Adicionar o prefixo ==============
+  if(coin_one.value == "BRL"){
+    var prefix_one = "R$";
+  }else if(coin_one.value == "USD"){
+    var prefix_one = "$";
+  }
+  if(coin_two.value == "BRL"){
+    var prefix_two = "R$";
+  }else if(coin_two.value == "USD"){
+    var prefix_two = "$";
+  }
+  var  value_one = prefix_one.concat(amountEl_one.value);
+  var value_two = prefix_two.concat(amountEl_two.value);
+  
+  
+  
   var doc = new jsPDF()
   doc.setFontSize(22);
-  
   doc.setFont("helvetica", "bold");
   doc.text("Relatório", 105, 10, null, null, "center");
-  
+
+    //=============== Gerando o PDF =============================
   doc.setFontSize(16);
   doc.setFont("helvetica", "normal");
   doc.text("Nome: " + name.value, 10, 30);
-  doc.text("Valor a ser convertido: " + amountEl_one.value,  10, 50);
+  doc.text("Valor a ser convertido: ",  10, 50);
+  doc.text(value_one, 80, 50);
   doc.text("Moeda de entrada: " + currencyEl_one.value, 145, 50);
-  
-  doc.text("Valor Após a conversão: " + amountEl_two.value, 10, 70);
+  doc.text("Valor Após a conversão: ", 10, 70);
+  doc.text(value_two, 80, 70)
   doc.text("Moeda de saída: " + currencyEl_two.value, 145, 70);
-  doc.text("Data da operação: ", 10, 100);
+  doc.text("Data da operação: " + diaF + '/'+ mesF + '/' + anoF, 10, 100);
   doc.save('A4.pdf');
   }
 
